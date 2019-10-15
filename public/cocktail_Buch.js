698e01713e837a39e3eac15f1d2c0f8c7ca487a1
nicojose <nicojose.neitzert@gmail.com>
2019-10-15 16:22:18 +0200

    asdf

--- public/cocktail_Buch.js
+++ public/cocktail_Buch.js
@@ -1,3 +1,23 @@
-fetch("https://swapi.co/api/people/") // request object
-.then(response => respose.json()) // response object
-.then(json => console.log(json));
+function showCocktails(event){
+    event.preventDefault();
+
+    document.getElementById("savedContainer").classList.remove("hidden");
+    document.getElementById("speichern").style = "display:none";
+
+    const values = Object.fromEntries(new FormData(event.target));
+    console.log(values);
+
+    fetch('/cocktails', {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(values)
+      }).then(res => {
+        return res.json();
+      }).then(todo => {
+        createListItem(todo);
+      });
+}
+
+document.querySelector('#showCocktail').addEventListener('submit', showCocktails)
